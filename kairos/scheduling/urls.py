e5f736e83e3d09b509d3cc1818d8c4eeedb4f544
Miguel Angel Calvache Giraldo <calvache116@gmail.com>
2023-03-20 17:31:30 -0500

    Todos los html, paths y views creados

--- kairos/scheduling/urls.py
+++ kairos/scheduling/urls.py
@@ -3,5 +3,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-]
+    path('', views.login_page, name='login'),
+    
+    #HOME PAGES
+    path('manager/',views.home_manager,name='home_manager'),
+    path('admin/',views.home_admin,name='home_admin'),
+    path('adviser/',views.home_adviser,name='home_adviser'),
+    path('operator/',views.home_operator,name='home_operator'),
+
+    # FUNCIONALITIES
+    path('reports/',views.reports,name='reports'),
+    path('block_schedule/',views.block_schedule,name='block_schedule'),
+    path('create_user/',views.create_user,name='create_user'),
+    path('delete_user/',views.delete_user,name='delete_user'),
+    path('see_schedules/',views.see_schedules,name='see_schedules'),
+    path('asign_turns/',views.asign_turns,name='asign_turns'),
+    path('modify_schedules/',views.modify_schedules,name='modify_schedules'),
+    path('validate_service_provided/',views.validate_service_provided,name='validate_service_provided'),
+
+]   
--- kairos/scheduling/views.py
+++ kairos/scheduling/views.py
@@ -4,5 +4,58 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def login_page(request):
+    if request.method == 'GET':
+        return render(request, template_name='login.html')
+    
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     user = authenticate(username=username, password=password)
+
+    # if user is not None:
+    #     login(request, user)
+    #     messages.success(request, f'¡Bienvenido {user.username}, nos encanta tenerte de vuelta!')
+    #     return redirect('home')
+    # else:
+    #     messages.error(request, '¡El usuario y la contraseña no coinciden, por favor vuelva a intentarlo!')
+    #     return redirect('login')
+
+
+    return render(request,template_name="login.html")
+
+def home_manager(request):
+    return render(request,template_name="0-manager/home-manager.html")
+
+def home_admin(request):
+    return render(request,template_name="1-admin/home-admin.html")
+
+def home_adviser(request):
+    return render(request,template_name="2-adviser/home-adviser.html")
+
+def home_operator(request):
+    return render(request,template_name="3-operator/home-operator.html")
+
+def block_schedule(request):
+    return render(request,template_name="0-1-block_schedule.html")
+
+def create_user(request):
+    return render(request,template_name="0-1-create_user.html")
+
+def delete_user(request):
+    return render(request,template_name="0-1-delete_user.html")
+
+def reports(request):
+    return render(request,template_name="0-reports.html")
+
+def see_schedules(request):
+    return render(request,template_name="1-2-3-see_schedules.html")
+
+def asign_turns(request):
+    return render(request,template_name="1-2-asign_turns.html")
+
+def modify_schedules(request):
+    return render(request,template_name="1-modify_schedules.html")
+
+def validate_service_provided(request):
+    return render(request,template_name="1-validate_service_provided.html")
